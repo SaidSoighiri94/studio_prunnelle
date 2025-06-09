68acf378890a2302be5d19e0576ac4f0892ab9dd
SaidSoighiri94 <msaidsoighir@gmail.com>
2025-06-09 16:26:26 +0200

    Ajout de la securisation du site

--- src/DataFixtures/UserFixures.php
+++ src/DataFixtures/UserFixures.php
@@ -4,11 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Faker\Factory;
+use App\Entity\Addresse;
 
 class UserFixures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // Creation d'une instance faker avec le local francais
+
+        $faker = Factory::create('fr_FR');
+
+        //Creation d'un utilisateur admin
+        //$admin = (new User())
+         //   ->set
         // $product = new Product();
         // $manager->persist($product);
 
