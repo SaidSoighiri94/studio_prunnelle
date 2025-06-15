8db7981e7d09c61840e264af2b1cf477408cbbd0
SaidSoighiri94 <msaidsoighir@gmail.com>
2025-06-15 17:21:37 +0200

    creation du controller du export pdf

---
+++ src/Controller/ExportPdfController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ecole;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Knp\Snappy\Pdf;
+use Twig\Environment;
+
+final class ExportPdfController extends AbstractController
+{
+    #[Route('ecole/{id}/export/pdf', name: 'app_export_pdf_ecole')] 
+    public function exportPdfEcole(Ecole $ecole, Pdf $knpSnappyPdf, Environment $twig): Response
+    {
+        // Render the HTML content for the PDF
+        $html = $twig->render('export_pdf/ecole.html.twig', [
+            'ecole' => $ecole,
+        ]);
+
+         $pdf = $knpSnappyPdf->getOutputFromHtml($html);
+
+        // Generate the PDF from the HTML content
+        return new PdfResponse ($pdf, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="ecole_' . $ecole->getId() . '.pdf"'
+        ]
+        );
+
+    }
+
+    #[Route('/export/pdf', name: 'app_export_pdf')]
+    public function index(): Response
+    {
+        return $this->render('export_pdf/index.html.twig', [
+            'controller_name' => 'ExportPdfController',
+        ]);
+    }
+}
